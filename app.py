d60229adbceffacb1dab2b8b00ef45380d1b16ec
bibby <bibby@bbby.org>
2017-01-09 13:09:05 -0800

    writing assignment generator. `/write/3`

--- app.py
+++ app.py
@@ -10,13 +10,28 @@ def index():
     return render_template('index.tpl')
 
 
+@app.route("/write/<int:num>")
+def pick_page(num):
+    return render_template('write.html', cards=random_cards(num))
+
+
+@app.route("/data/<int:num>")
+def data_feed_n(num):
+    return get_cards(num)
+
+
 @app.route("/data")
 def data_feed():
+    return get_cards()
+
+
+def get_cards(num=None):
     return Response(
-        response=json.dumps([c for c in random_cards()]),
+        response=json.dumps([c for c in random_cards(num)]),
         status=200,
         mimetype="application/json"
     )
+
 
 @app.route("/decks")
 def decks():
